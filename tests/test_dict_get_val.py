97e7b39e24c76ee63985740cd9263261e58b3590
Maxim Abavi <afghan1363@ya.kz>
2023-07-29 14:58:51 +0500

    writing fixture

--- tests/test_dict_get_val.py
+++ tests/test_dict_get_val.py
@@ -1,9 +1,12 @@
+import pytest
 from utils.dict import get_val
+@pytest.fixture
+def dict_for_test():
+    return {"lang": "Python", "version": "3.11"}
+# dict_for_test = {"lang": "Python", "version": "3.11"}
 
-dict_for_test = {"lang": "Python", "version": "3.11"}
 
-
-def test_get_val():
+def test_get_val(dict_for_test):
     assert get_val(dict_for_test, "lang") == "Python"
     assert get_val(dict_for_test, "version") == "3.11"
     assert get_val(dict_for_test, "weather on monday") == "git"
